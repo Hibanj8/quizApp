2910738feeedbac3772a63b49fca955d801c61d4
hossamdev <73648971+hossam-dev14@users.noreply.github.com>
2023-12-22 15:49:36 +0100

    Add files via upload

--- src/compoments/Nav.js
+++ src/compoments/Nav.js
@@ -10,17 +10,12 @@ import { AiOutlineArrowRight } from "react-icons/ai";
       return (
   <nav className="navbar navbar-expand-lg navbar-dark" >
       <div className="container-fluid px-5 ">
-        <a className="navbar-brand fs-3 " href="#">Dev Recruit</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul   className="navbar-nav ms-auto">
-            <li  className="nav-item ps-5 "><Link to="/" className="nav-link active" >Home</Link></li>
-            <li  className="nav-item ps-5"><Link to="/Rules" className="nav-link active" >Rules</Link></li>
-            <li  className="nav-item ps-5"><Link to="/Quiz" className="nav-link active" >Quiz</Link></li>
-          </ul>
-      </div>
+        <a className="navbar-brand fs-3 " href="#">Quiz Tech</a>
+        <ul   className="navbar-nav ms-auto" style={{display:"flex", flexDirection: "row"}}>
+          <li  className="nav-item ps-5 "><Link to="/" className="nav-link active" >Home</Link></li>
+          <li  className="nav-item ps-5"><Link to="/Rules" className="nav-link active" >Rules</Link></li>
+          <li  className="nav-item ps-5"><Link to="/Quiz" className="nav-link active" >Quiz</Link></li>
+        </ul>
     </div>
   </nav>  
 
