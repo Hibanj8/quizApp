0941c05157868562b69fe1bb31f0a7d44a0b125c
Hibanj8 <153021131+Hibanj8@users.noreply.github.com>
2023-12-22 11:56:35 +0100

    first commit

---
+++ src/compoments/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Nav.css";
+import { Link } from "react-router-dom";
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+
+
+ function Nav(){
+      return (
+  <nav className="navbar navbar-expand-lg navbar-dark" >
+      <div className="container-fluid px-5 ">
+        <a className="navbar-brand fs-3 " href="#">Quiz Tech</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul   className="navbar-nav ms-auto">
+            <li  className="nav-item ps-5 "><Link to="/" className="nav-link active" >Home</Link></li>
+            <li  className="nav-item ps-5"><Link to="/Rules" className="nav-link active" >Rules</Link></li>
+            <li  className="nav-item ps-5"><Link to="/Quiz" className="nav-link active" >Quiz</Link></li>
+          </ul>
+      </div>
+    </div>
+  </nav>  
+
+  );
+    }
+    export default Nav;
